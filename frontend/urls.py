b882de88cb1228369e50f37ace901f1c097e8b66
Mitchell <s320289@students.cdu.edu.au>
2025-03-31 21:20:04 +1100

    Added basic image/heading through css file

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.project_list, name='project_list'),
-    path('projects/<int:id>/', views.project_details, name='project_details'),
     path('about/', views.about, name='about'),
 ]
