32f6a0140beeecd187fc46083ec06c450745753a
Mitchell <s320289@students.cdu.edu.au>
2025-04-02 12:01:04 +1100

    Updated AboutUs to remove HTML hardcoding

--- mango_pests_app/views.py
+++ mango_pests_app/views.py
@@ -7,4 +7,11 @@ def project_list(request):
     return render(request, 'mango_pests_app/project_list.html', {'page_title': 'Diseases & Pests'})
 
 def about(request):
-    return render(request, 'mango_pests_app/about.html', {'page_title': 'About Us'})
+    page_title = "About Us"  
+    team_members = [
+        {'name': 'Mitchell Danks', 'student_id': 'S320289', 'image': 'aboutmitchell.png'},
+        {'name': 'Benjamin Denison-Love', 'student_id': 'S330803', 'image': 'aboutbenjamin.png'},
+        {'name': 'Quang Thuan Nguyen', 'student_id': 'S370553', 'image': 'aboutquang.png'},
+        {'name': 'Spencer Siu', 'student_id': 'S344930', 'image': 'aboutspencer.png'}
+    ]
+    return render(request, 'mango_pests_app/about.html', {'page_title': page_title, 'team_members': team_members})
