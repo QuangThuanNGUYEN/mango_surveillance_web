803d27c4f9f087e4dbe53b84629fc9e734c572fb
Mitchell <s320289@students.cdu.edu.au>
2025-04-02 12:08:57 +1100

    Updated D&P list to remove hard HTML coding

--- mango_pests_app/views.py
+++ mango_pests_app/views.py
@@ -4,7 +4,16 @@ def home(request):
     return render(request, 'mango_pests_app/home.html', {'page_title': 'Home'})
 
 def project_list(request):
-    return render(request, 'mango_pests_app/project_list.html', {'page_title': 'Diseases & Pests'})
+    projects = [
+        {'name': 'Project 1', 'description': 'Description of Project 1', 'image': 'mangodp1.png'},
+        {'name': 'Project 2', 'description': 'Description of Project 2', 'image': 'mangodp2.png'},
+        {'name': 'Project 3', 'description': 'Description of Project 3', 'image': 'mangodp3.png'},
+        {'name': 'Project 4', 'description': 'Description of Project 4', 'image': 'mangodp4.png'},
+        {'name': 'Project 5', 'description': 'Description of Project 5', 'image': 'mangodp5.png'},
+        {'name': 'Project 6', 'description': 'Description of Project 6', 'image': 'mangodp6.png'},
+        {'name': 'Project 7', 'description': 'Description of Project 7', 'image': 'mangodp7.png'},
+    ]
+    return render(request, 'mango_pests_app/project_list.html', {'page_title': 'Diseases & Pests', 'projects': projects})
 
 def about(request):
     page_title = "About Us"  
