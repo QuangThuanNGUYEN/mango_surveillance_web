b80c60540175c7944d620c7224a54c16f5876fb6
Mitchell <s320289@students.cdu.edu.au>
2025-04-02 15:40:43 +1100

    Added dynamic page for dps

--- mango_pests_app/urls.py
+++ mango_pests_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.project_list, name='project_list'),
+    path('project/<str:project_name>/', views.project_details, name='project_details'),
     path('about/', views.about, name='about'),
 ]
