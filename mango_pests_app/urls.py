1ccd80e527c0e83fad094182667c12f57d3013e7
Spencer <s344930@students.cdu.edu.au>
2025-04-03 04:51:08 +0930

    Added data.py to improve our data handling. Altered urls.py and views.py to use django slug-field. TLDR : Task 3 stuff

--- mango_pests_app/urls.py
+++ mango_pests_app/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('projects/', views.project_list, name='project_list'),
-    path('project/<str:project_name>/', views.project_details, name='project_details'),
+    path('project_list/', views.project_list, name='project_list'),
+    
+    # path('project_list/<str:project_name>/', views.project_details, name='project_details'),
+    re_path(r'^project_list/(?P<project_name>[\w-]+)/$', views.project_details, name='project_details'),
+
+    
     path('about/', views.about, name='about'),
 ]
--- mango_pests_app/views.py
+++ mango_pests_app/views.py
@@ -1,34 +1,42 @@
 from django.shortcuts import render, get_object_or_404
+from .data import projects
 
 # Home page 
 def home(request):
     return render(request, 'mango_pests_app/home.html', {'page_title': 'Home'})
 
 # Project List
+# def project_list(request):
+#     projects = [
+#         {'name': 'Anthracnose', 'description': 'Description of Project 1', 'image': 'Anthracnose.png', 'details': 'Full details of Anthracnose'},
+#         {'name': 'Bacterial Black Spot', 'description': 'Description of Project 2', 'image': 'bacterialblackspot.png', 'details': 'Full details of Bacterial Black Spot'},
+#         {'name': 'Mango Scab', 'description': 'Description of Project 3', 'image': 'mangoscab.png', 'details': 'Full details of Mango Scab'},
+#         {'name': 'Mango Scale', 'description': 'Description of Project 4', 'image': 'mangoscale.png', 'details': 'Full details of Mango Scale'},
+#         {'name': 'Fruit Fly', 'description': 'Description of Project 5', 'image': 'fruitfly.png', 'details': 'Full details of Fruit Fly'},
+#         {'name': 'Mango Leafhoppers', 'description': 'Description of Project 6', 'image': 'mangoleafhopper.png', 'details': 'Full details of Mango Leafhoppers'},
+#         {'name': 'Mango Seed Weevil', 'description': 'Description of Project 7', 'image': 'seedweevil.png', 'details': 'Full details of Mango Seed Weevil'},
+#     ]
+#     return render(request, 'mango_pests_app/project_list.html', {'page_title': 'Diseases & Pests', 'projects': projects})
+
 def project_list(request):
-    projects = [
-        {'name': 'Anthracnose', 'description': 'Description of Project 1', 'image': 'Anthracnose.png', 'details': 'Full details of Anthracnose'},
-        {'name': 'Bacterial Black Spot', 'description': 'Description of Project 2', 'image': 'bacterialblackspot.png', 'details': 'Full details of Bacterial Black Spot'},
-        {'name': 'Mango Scab', 'description': 'Description of Project 3', 'image': 'mangoscab.png', 'details': 'Full details of Mango Scab'},
-        {'name': 'Mango Scale', 'description': 'Description of Project 4', 'image': 'mangoscale.png', 'details': 'Full details of Mango Scale'},
-        {'name': 'Fruit Fly', 'description': 'Description of Project 5', 'image': 'fruitfly.png', 'details': 'Full details of Fruit Fly'},
-        {'name': 'Mango Leafhoppers', 'description': 'Description of Project 6', 'image': 'mangoleafhopper.png', 'details': 'Full details of Mango Leafhoppers'},
-        {'name': 'Mango Seed Weevil', 'description': 'Description of Project 7', 'image': 'seedweevil.png', 'details': 'Full details of Mango Seed Weevil'},
-    ]
     return render(request, 'mango_pests_app/project_list.html', {'page_title': 'Diseases & Pests', 'projects': projects})
 
 # Project Details 
+# def project_details(request, project_name):
+#     projects = [
+#         {'name': 'Anthracnose', 'description': 'Description of Project 1', 'image': 'Anthracnose.png', 'details': 'Full details of Anthracnose'},
+#         {'name': 'Bacterial Black Spot', 'description': 'Description of Project 2', 'image': 'bacterialblackspot.png', 'details': 'Full details of Bacterial Black Spot'},
+#         {'name': 'Mango Scab', 'description': 'Description of Project 3', 'image': 'mangoscab.png', 'details': 'Full details of Mango Scab'},
+#         {'name': 'Mango Scale', 'description': 'Description of Project 4', 'image': 'mangoscale.png', 'details': 'Full details of Mango Scale'},
+#         {'name': 'Fruit Fly', 'description': 'Description of Project 5', 'image': 'fruitfly.png', 'details': 'Full details of Fruit Fly'},
+#         {'name': 'Mango Leafhoppers', 'description': 'Description of Project 6', 'image': 'mangoleafhopper.png', 'details': 'Full details of Mango Leafhoppers'},
+#         {'name': 'Mango Seed Weevil', 'description': 'Description of Project 7', 'image': 'seedweevil.png', 'details': 'Full details of Mango Seed Weevil'},
+#     ]
+#     project = next((item for item in projects if item['name'].lower() == project_name.lower()), None)
+#     return render(request, 'mango_pests_app/project_details.html', {'project': project})
+
 def project_details(request, project_name):
-    projects = [
-        {'name': 'Anthracnose', 'description': 'Description of Project 1', 'image': 'Anthracnose.png', 'details': 'Full details of Anthracnose'},
-        {'name': 'Bacterial Black Spot', 'description': 'Description of Project 2', 'image': 'bacterialblackspot.png', 'details': 'Full details of Bacterial Black Spot'},
-        {'name': 'Mango Scab', 'description': 'Description of Project 3', 'image': 'mangoscab.png', 'details': 'Full details of Mango Scab'},
-        {'name': 'Mango Scale', 'description': 'Description of Project 4', 'image': 'mangoscale.png', 'details': 'Full details of Mango Scale'},
-        {'name': 'Fruit Fly', 'description': 'Description of Project 5', 'image': 'fruitfly.png', 'details': 'Full details of Fruit Fly'},
-        {'name': 'Mango Leafhoppers', 'description': 'Description of Project 6', 'image': 'mangoleafhopper.png', 'details': 'Full details of Mango Leafhoppers'},
-        {'name': 'Mango Seed Weevil', 'description': 'Description of Project 7', 'image': 'seedweevil.png', 'details': 'Full details of Mango Seed Weevil'},
-    ]
-    project = next((item for item in projects if item['name'].lower() == project_name.lower()), None)
+    project = next((item for item in projects if item['slug'] == project_name), None)
     return render(request, 'mango_pests_app/project_details.html', {'project': project})
 
 # About
