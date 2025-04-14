b13207569c9b4a55608f9724f411db4b03d24939
Thuan Nguyen <thuanquangnguyen2003@gmail.com>
2025-04-14 17:53:19 +0930

    split css files for each html template for easier maintenance

--- mango_pests_app/views.py
+++ mango_pests_app/views.py
@@ -12,42 +12,13 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['page_title'] = 'Home'
         context['heading'] = "Welcome to the World Mango Organisation of Group 28 of HIT237!"
         context['description'] = "We are dedicated to combating mango pests and diseases through research and awareness. Browse our site to learn more about the pests that are consuming our beloved mangos!"
         return context
 
 
+
 # Threat List Page
-# class ThreatListView(TemplateView):
-#     template_name = 'mango_pests_app/threat_list.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         query = self.request.GET.get('q', '').lower()
-#         threat_type = self.request.GET.get('category', '').lower()  # Get category filter
-
-#         if query:
-#             filtered_threats = [t for t in mango_threats if query in t.name.lower()]
-#         else:
-#             filtered_threats = mango_threats
-
-#         if threat_type in ['pest', 'disease']:
-#             filtered_threats = [t for t in filtered_threats if t.threat_type == threat_type]
-
-        
-#         paginator = Paginator(filtered_threats, 10)  # Show 10 items per page
-#         page_number = self.request.GET.get('page')  # Get the current page number from the URL
-#         page_obj = paginator.get_page(page_number)
-
-#         # Add context variables
-#         context['page_title'] = 'Pests and Diseases'
-#         context['page_obj'] = page_obj  # Pass the paginated objects to the template
-#         context['query'] = query
-#         context['threat_type'] = threat_type 
-#         return context
-    
-    
 class ThreatListView(TemplateView):
     template_name = 'mango_pests_app/threat_list.html'
 
@@ -130,10 +101,10 @@ class ThreatDetailView(View):
         if threat:
             return render(request, 'mango_pests_app/threat_details.html', {'threat': threat})
         else:
-            # return render(request, 'mango_pests_app/404.html')
             raise Http404("Threat not found")
 
 
+# Compare Threats Page
 class CompareThreatsView(View):
     MAX_SELECTIONS = 3  # Limit the number of threats that can be compared
 
--- mango_surveillance_web/urls.py
+++ mango_surveillance_web/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('mango_pests_app.urls')),
+    re_path(r"admin/", admin.site.urls),
+    re_path(r'', include('mango_pests_app.urls')),
 ]
+
